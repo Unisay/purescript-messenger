64f8584f5052382699381b76eae94e913d15b5e2
Yuriy Lazarev <unisay@users.noreply.github.com>
2021-09-29 08:10:15 +0200

    backend: ServerM

--- backend/src/Main.purs
+++ backend/src/Main.purs
@@ -3,19 +3,25 @@ module Main where
 import Prelude
 
 import Control.Monad.Error.Class (throwError)
-import Control.Monad.Except (runExcept)
+import Control.Monad.Except (ExceptT, mapExceptT, runExcept, runExceptT)
+import Control.Monad.Trans.Class (lift)
+import Data.Array as Array
+import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
+import Data.Identity (Identity)
+import Data.List.NonEmpty (NonEmptyList)
+import Data.Newtype (class Newtype, over, unwrap, wrap)
 import Effect (Effect)
 import Effect.Class.Console (log)
-import Foreign (renderForeignError)
+import Foreign (ForeignError, MultipleErrors, renderForeignError)
 import Foreign as Foreign
 import Foreign.Generic.Class (class Decode)
 import Middleware as Middleware
 import Node.Express.App (App, get, listenHttp, post)
-import Node.Express.Handler (Handler)
+import Node.Express.Handler (Handler, HandlerM)
 import Node.Express.Request as Request
 import Node.Express.Response as Response
-import Data.Array as Array
+import Unsafe.Coerce (unsafeCoerce)
 
 main :: Effect Unit
 main = void $ listenHttp app tcpPort \_ -> log $ "Listening on " <> show tcpPort
@@ -36,29 +42,57 @@ instance decodeLoginReason :: Decode LogoutReason where
 
 type LogoutRequest = { reason :: LogoutReason }
 
+newtype ServerM a = ServerM (HandlerM (ExceptT MultipleErrors Identity a))
+
+instance Functor ServerM where
+  map f = over ServerM (map (map f))
+
+instance Apply ServerM where
+  apply = unsafeCoerce "Homework"
+
+instance Applicative ServerM where
+  pure = unsafeCoerce "Homework"
+
+instance Bind ServerM where
+  bind = unsafeCoerce "Homework"
+
+derive instance newtypeServerM :: Newtype (ServerM a) _
+
+runServerM :: forall a. ServerM a -> HandlerM (ExceptT MultipleErrors Identity a)
+runServerM = unwrap
+
 app :: App
 app = do
   Middleware.init
   get "/" do
     Response.send "Messenger API"
-  post "/login" $ withBody \loginRequest -> do
-    if login loginRequest then Response.send "Success"
-    else do
-      Response.setStatus 403
-      Response.send "Failure"
-  post "/logout" $ withBody \(logoutRequest :: LogoutRequest) ->
-    case logoutRequest.reason of
-      Timeout -> Response.send "Logout successful: timeout."
-      UserAction -> Response.send "Logout successful: bye bye!"
+  -- post "/login" $ withBody \loginRequest -> do
+  --   if login loginRequest then Response.send "Success"
+  --   else do
+  --     Response.setStatus 403
+  --     Response.send "Failure"
+  post "/logout" $ handleErrors $ runServerM $ do
+    logoutRequest :: LogoutRequest <- wrap Request.getBody
+    pure unit
+    -- wrap case logoutRequest.reason of
+    --   Timeout -> Response.send "Logout successful: timeout."
+    --   UserAction -> Response.send "Logout successful: bye bye!"
 
-withBody :: forall body. Decode body => (body -> Handler) -> Handler
-withBody cont =
-  Request.getBody >>= runExcept >>> case _ of
-    Left multipleErrors -> do
-      Response.setStatus 400
-      Response.send $ Array.fromFoldable $ map renderForeignError multipleErrors 
-    Right body ->
-      cont body
+handleErrors :: HandlerM (ExceptT MultipleErrors Identity Unit) -> HandlerM Unit
+handleErrors handlerHasErrors = handlerHasErrors >>= runExcept >>> case _ of
+  Left multipleErrors -> do
+    Response.setStatus 400
+    Response.send $ Array.fromFoldable $ map renderForeignError multipleErrors
+  Right _ -> pure unit
+
+-- withBody :: forall body. Decode body => (body -> Handler) -> Handler
+-- withBody cont =
+--   Request.getBody >>= runExcept >>> case _ of
+--     Left multipleErrors -> do
+--       Response.setStatus 400
+--       Response.send $ Array.fromFoldable $ map renderForeignError multipleErrors
+--     Right body ->
+--       cont body
 
 -- Business Logic:
 login :: LoginRequest -> Boolean
