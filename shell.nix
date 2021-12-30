b73925ff1d0b88959506c4ad1a9de95b633af61f
Yuriy Lazarev <Unisay@users.noreply.github.com>
2021-12-30 16:06:12 +0000

    update easy-purescript-nix

--- shell.nix
+++ shell.nix
@@ -13,8 +13,8 @@ let
     (pkgs.fetchFromGitHub {
       owner = "justinwoo";
       repo = "easy-purescript-nix";
-      rev = "7802db65618c2ead3a55121355816b4c41d276d9";
-      sha256 = "0n99hxxcp9yc8yvx7bx4ac6askinfark7dnps3hzz5v9skrvq15q";
+      rev = "7f0674685367c087dbc6444d3d6429638f73543d";
+      sha256 = "1ijvl8djvx212d7kjlif7vfyqaikgjwda0irrkxznzy9nz9sx6b4";
     })
     { inherit pkgs; };
 
