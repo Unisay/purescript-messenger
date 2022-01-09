424130b87e7b950ec1ce1f795745f25e3559871c
Yuriy Lazarev <Unisay@users.noreply.github.com>
2022-01-09 17:03:46 +0000

    CORS for Signing form

--- backend/src/Middleware.js
+++ backend/src/Middleware.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const express = require("express");
-
 exports.json = express.json();
-exports.cors = require("cors")();
+
+const corsOptions = {
+  origin: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+exports.cors = require("cors")(corsOptions);
