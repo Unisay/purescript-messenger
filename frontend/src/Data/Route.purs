722db4bc8f8785f0b25dd1da40285c8ef9e8b9b6
VadymVadymov <VadymVadymov@users.noreply.github.com>
2022-01-22 17:07:20 +0200

    Route profile add

--- frontend/src/Data/Route.purs
+++ frontend/src/Data/Route.purs
@@ -2,10 +2,14 @@ module Data.Route where
 
 import Prelude
 
+import Data.Username (Username, parse, toString)
+import Data.Array.NonEmpty (fold1)
+import Data.Bifunctor (lmap)
+import Data.Either (Either)
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe)
 import Data.Show.Generic (genericShow)
-import Routing.Duplex (RouteDuplex', optional, path, root)
+import Routing.Duplex (RouteDuplex', as, optional, path, root, segment)
 import Routing.Duplex.Generic as G
 
 -- https://github.com/natefaubion/purescript-routing-duplex/blob/v0.2.0/README.md
@@ -26,5 +30,14 @@ route = optional $ root $ G.sum
   { "Home": G.noArgs
   , "SignIn": path "signin" G.noArgs
   , "SignUp": path "signup" G.noArgs
-  -- , "Profile": path "profile" (username segment)
+  , "Profile": path "profile" (username segment)
   }
+
+usernameToString :: Username -> String
+usernameToString = toString
+
+stringToUsername :: String -> Either String Username
+stringToUsername str = lmap fold1 $ parse str
+
+username :: RouteDuplex' String -> RouteDuplex' Username
+username = as usernameToString stringToUsername
