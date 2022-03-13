30b49a7216d4d5df8e0e4ac85bd464b8aa7083ce
Yuriy Lazarev <Unisay@users.noreply.github.com>
2022-03-13 13:38:27 +0000

    refactor: spec

--- frontend/test/Component/NotificationsSpec.purs
+++ frontend/test/Component/NotificationsSpec.purs
@@ -5,6 +5,7 @@ import Prelude
 import Component.Notifications (Action(..), evalSpec, initialState)
 import Control.Monad.State.Class (gets)
 import Data.Notification (critical, important, useful)
+import Data.Traversable (traverse_)
 import Effect.Aff.Class (class MonadAff, liftAff)
 import Effect.Class (liftEffect)
 import Halogen.Driver (runComponent)
@@ -15,17 +16,15 @@ import Test.Unit.Assert (shouldEqual)
 spec ∷ TestSuite
 spec = test "Notifications" do
   { emitter, listener } ← liftEffect Subscription.create
-  let send = liftEffect <<< Subscription.notify listener
+  let sendNotification = liftEffect <<< Subscription.notify listener
 
-  runComponent (initialState emitter) evalSpec \action → do
-    send $ useful "u"
-    send $ important "i"
-    send $ critical "c"
-    action $ Close 1
+  runComponent (initialState emitter) evalSpec \simulateAction → do
+    traverse_ sendNotification [ useful "u", important "i", critical "c" ]
+    simulateAction $ Close 1
     gets _.queue >>= shouldBe [ critical "c", useful "u" ]
-    action $ Close 0
+    simulateAction $ Close 0
     gets _.queue >>= shouldBe [ useful "u" ]
-    action $ Close 0
+    simulateAction $ Close 0
     gets _.queue >>= shouldBe []
 
 shouldBe ∷ ∀ a m. MonadAff m ⇒ Eq a ⇒ Show a ⇒ a → a → m Unit
