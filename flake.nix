06a8dedbe34419e0fab2f563342732dd2735c257
Vadym Vadymov <vadymvadymov@users.noreply.github.com>
2022-03-23 15:04:52 +0100

    fix: zephyr cut off

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
     flake-compat.url = github:edolstra/flake-compat;
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    zephyr.flake = false;
-    zephyr.url = "https://github.com/MaybeJustJames/zephyr/releases/download/c074270/Linux.tar.gz";
+    # zephyr.flake = false;
+    # zephyr.url = "https://github.com/MaybeJustJames/zephyr/releases/download/c074270/Linux.tar.gz";
   };
 
-  outputs = { self, nixpkgs, flake-compat, flake-utils, zephyr }:
+  outputs = { self, nixpkgs, flake-compat, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in
@@ -19,15 +19,15 @@
           pkgs.mkShell {
             buildInputs = with pkgs; [
               dhall
-              httpie
               nixpkgs-fmt
               nodejs-16_x
               purescript
               spago
+              nixpkgs-fmt
             ];
             shellHook = ''
               export LC_ALL=C
-              export PATH=${zephyr}:$PATH
+              export PATH=$PATH
             '';
           };
       });
