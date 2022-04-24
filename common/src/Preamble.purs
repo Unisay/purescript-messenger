eb82ca31c98c04ca84af3fd56e8b6013edf1d406
Yura Lazarev <Unisay@users.noreply.github.com>
2022-04-24 20:29:20 +0200

    Preamble: pass

--- common/src/Preamble.purs
+++ common/src/Preamble.purs
@@ -4,11 +4,15 @@ module Preamble
   , module Data.Either
   , module Effect
   , module Effect.Class.Console
+  , pass
   ) where
 
 import Prelude
 
-import Data.Maybe (Maybe(..), fromJust, fromMaybe, isJust, isNothing, maybe)
 import Data.Either (Either(..), either, hush, note)
+import Data.Maybe (Maybe(..), fromJust, fromMaybe, isJust, isNothing, maybe)
 import Effect (Effect)
 import Effect.Class.Console (log, logShow)
+
+pass ∷ ∀ t. Applicative t ⇒ t Unit
+pass = pure unit
