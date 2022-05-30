9d033087b820817c642871ef66901880c2dd536d
VadymVadymov <vadymvadymov@users.noreply.github.com>
2022-05-30 21:33:14 +0200

    feat: ability to encode `DateTime`

---
+++ common/src/Data/Data.JsonTime.purs
@@ -0,0 +1,36 @@
+module Data.JsonTime
+  ( EncodeDateTime(..)
+  ) where
+
+import Preamble
+
+import Data.Argonaut.Encode (class EncodeJson, encodeJson)
+import Data.Date (Date, day, month, year)
+import Data.Date.Component (Month(..))
+import Data.Enum (fromEnum)
+import Data.Time (Time, hour, minute, second)
+
+data EncodeDateTime = DateTime Date Time
+
+instance EncodeJson EncodeDateTime where
+  encodeJson (DateTime date time) = encodeJson
+    { date: (show (fromEnum $ year date)) <> showMonth (month date) <>
+        show (fromEnum $ day date)
+    , time: show $ fromEnum (second time) + (fromEnum (minute time) * 60)
+        + (fromEnum (hour time) * 3600)
+    }
+
+showMonth ∷ Month → String
+showMonth = case _ of
+  January → "01"
+  February → "02"
+  March → "03"
+  April → "04"
+  May → "05"
+  June → "06"
+  July → "07"
+  August → "08"
+  September → "09"
+  October → "10"
+  November → "11"
+  December → "12"
