d6482a17e5ab7870de7bb4a2904050ec5f075d37
Yura Lazarev <Unisay@users.noreply.github.com>
2022-08-20 21:22:00 +0200

    Don't run HTTPS in production, use caches for purescript

--- server.mjs
+++ server.mjs
@@ -1,14 +1,22 @@
 import express from "express";
 import * as fs from 'node:fs';
+import * as http from 'http';
 import * as https from 'https';
 
 const port = process.env.PORT || 8080;
-const certs = {
-  key: fs.readFileSync('ssl/key.pem'),
-  cert: fs.readFileSync('ssl/cert.crt')
+const app = express();
+app.use(express.static('dist'));
+
+if (process.env.NODE_ENV != 'production') {
+  const certs = {
+    key: fs.readFileSync('ssl/key.pem'),
+    cert: fs.readFileSync('ssl/cert.crt')
+  };
+  https.createServer(certs, app).listen(port, () => {
+    console.log(`HTTPS server is listening on port ${port}`)
+  });
+} else {
+  http.createServer(app).listen(port, () => {
+    console.log(`HTTP server is listening on port ${port}`)
+  });
 };
-const server = express();
-server.use(express.static('dist'));
-https.createServer(certs, server).listen(port, () => {
-  console.log(`HTTP server is listening on port ${port}`)
-})
